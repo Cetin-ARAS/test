ad3e96ff0197a4462a2041b135d33d38fd44a630
ÇETİN <cetinnnarasss@gmail.com>
2022-12-18 13:08:26 +0300

    math

---
+++ src/math.js
@@ -0,0 +1,20 @@
+const fahrenheitToCelsius = (temp) => (temp - 32) / 1.8;
+
+const celciusToFahreneit = (temp) => temp * 1.8 + 32;
+
+const calculateTotal = (price, vat = 0.18) => price * (1 + vat);
+
+const fibonacci = ( um = 1) => {
+    if (num < 1) {
+        throw new Error("Must enter a number greater than "0)
+    }
+
+    const series = [1, 1];
+    for(let i = 2; i< num, i++){
+        const a = series[i - 1];
+        const b = series[i - 2];
+        series.push(a + b);
+    }
+    
+    return series{num - 1};
+}
