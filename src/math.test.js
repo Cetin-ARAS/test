4862a1ca8fdb626e60eafbf7649936643c863d27
ÇETİN <cetinnnarasss@gmail.com>
2022-12-18 14:19:01 +0300

    all

--- src/math.test.js
+++ src/math.test.js
@@ -10,17 +10,31 @@ describe('calculateTotal', () =>{
   it('should exist', () => {
     expect(calculateTotal).toBeDefined();
   });
-  it('should return a number', () ={
-    
-  })
+  it('should return a number', () => {
+    expect(calculateTotal(100, 0.20)).toBe(120);
+  });
+  it('should return with default VAT', () => {
+    expect(calculateTotal(200)).toBe(236);
+  });
+
 });
 
-describe('celsiusToFahrenheit', () => {
-    
+describe('celsiusToFahrenheit', () => {  
+ it('should exist', () => {
+    expect(celsiusToFahrenheit).toBeDefined();
+         }); 
 })
+
 describe('fahrenheitToCelsius', () => {
+  it('should exist', () => {
+     expect(fahrenheitToCelsius).toBeDefined();
+      });
+    
     
 })
 describe('fibonacci', () => {
+  it('should exist', () => {
+     expect(fibonacci).toBeDefined();
+      });
     
 })
