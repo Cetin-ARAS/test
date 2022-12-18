130ae8c5516ffc3fde177a44202c27da4734ee4a
ÇETİN <cetinnnarasss@gmail.com>
2022-12-18 14:00:24 +0300

    math.test.js

---
+++ src/math.test.js
@@ -0,0 +1,26 @@
+//! node module import formatı
+
+const { calculateTotal, celsiusToFahrenheit, fahrenheitToCelsius, fibonacci } = require('./math')
+
+//! es6 module format
+// import{ calculateTotal, celciusToFahrenheit, fahrenheitToCelsius, fibonacci }from './math';
+
+describe('calculateTotal', () =>{
+  // test caselerini oluşturuyoruz
+  it('should exist', () => {
+    expect(calculateTotal).toBeDefined();
+  });
+  it('should return a number', () ={
+    
+  })
+});
+
+describe('celsiusToFahrenheit', () => {
+    
+})
+describe('fahrenheitToCelsius', () => {
+    
+})
+describe('fibonacci', () => {
+    
+})
